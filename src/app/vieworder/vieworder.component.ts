25b4a6c82ac57c8f38fd4f7252a2f1f2d878928e
bansaltushar014 <bansaltushar014@gmail.com>
2020-02-22 21:12:29 +0530

    Edit and Delete Working

--- src/app/vieworder/vieworder.component.ts
+++ src/app/vieworder/vieworder.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vieworder',
@@ -8,14 +9,56 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ViewOrderComponent implements OnInit {
   closeResult: string;
-
+  formdata;
+  data = [
+    // {
+    //  "orderNumber" : "1",
+    //  "orderDueDate": "12/10/2020",
+    //  "customerBuyerName": "Tushar",
+    //  "customerAddress": "Address",
+    //  "customerPhone": "9997890909",
+    //  "orderTotal": "300"
+    // }
+  ];
+  random;
+  index;
+  customerBuyerName;
+  orderDueDate;
+  customerAddress;
+  customerPhone;
+  orderTotal;
 
   constructor(private modalService: NgbModal) { }
 
-  open(content) {
+  open(content,index) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log(index);
+      this.data.splice(index,1);
       this.closeResult = `Closed with: ${result}`;
       console.log("yes delete this");
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  openedit(content,index) {
+    this.orderDueDate = this.data[index].orderDueDate;
+    this.customerBuyerName = this.data[index].customerBuyerName;
+    this.customerAddress = this.data[index].customerAddress;
+    this.customerPhone = this.data[index].customerPhone;
+    this.orderTotal = this.data[index].orderTotal;
+    
+    console.log(this.customerBuyerName);
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log(index);
+      this.index = index;
+      this.data[index].orderDueDate = this.orderDueDate;
+      this.data[index].customerBuyerName = this.customerBuyerName;
+      this.data[index].customerAddress = this.customerAddress;
+      this.data[index].customerPhone = this.customerPhone;
+      this.data[index].orderTotal = this.orderTotal;
+      this.closeResult = `Closed with edit: ${result}`;
+       console.log("yes delete this");
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
@@ -33,20 +76,16 @@ export class ViewOrderComponent implements OnInit {
 
   
 
-  data = [
-    // {
-    //  "orderNumber" : "1",
-    //  "orderDueDate": "12/10/2020",
-    //  "customerBuyerName": "Tushar",
-    //  "customerAddress": "Address",
-    //  "customerPhone": "9997890909",
-    //  "orderTotal": "300"
-    // }
-  ];
-  random;
-
   ngOnInit() {
-    console.log(history.state[0]);
+   
+    this.formdata = new FormGroup({
+      orderDueDate: new FormControl(""),
+      customerBuyerName: new FormControl(""),
+      customerAddress: new FormControl(""),
+      customerPhone: new FormControl(""),
+      orderTotal: new FormControl("")
+    });
+   console.log(history.state[0]);
     this.random = history.state;
     // for(var i=0; i<this.random.length;i++){
     //   if(history.state[i]){
